904673be8d0ffb4aae7387d4be1e569d05da8589
Harsha Vardhan koneru <harsha.koneru@lsz.ads.valuelabs.net>
2019-05-28 14:33:43 +0530

    Added comments

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,18 +5,21 @@ const express = require('express');
 // except it is bound to an instance of express.Router().
 const router = express.Router();
 
+// Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Orders were fetched'
     })
 })
 
+// Handle incoming POST requests to /orders
 router.post('/', (req, res, next) => {
     res.status(201).json({
         message: 'Orders was created'
     })
 })
 
+// Handle incoming GET requests to /orders/:orderId
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order details',
@@ -24,14 +27,12 @@ router.get('/:orderId', (req, res, next) => {
     })
 })
 
+// Handle incoming DELETE requests to /orders/:orderId
 router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order deleted',
         orderId: req.params.orderId
     })
 })
-
-
-
 
 module.exports = router;
