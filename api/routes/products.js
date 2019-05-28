45f8d6ade474239ad629617d7cbfbdbbe1080d40
Harshavardhan Koneru <koneru.harshavardhan499@gmail.com>
2019-05-28 09:41:31 +0530

    Added producr routes

---
+++ api/routes/products.js
@@ -0,0 +1,46 @@
+// Importing express package
+const express = require('express');
+
+// Router-level middleware works in the same way as application-level middleware, 
+// except it is bound to an instance of express.Router().
+const router = express.Router();
+
+router.get('/',(req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/',(req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        })
+    } else{
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    })
+})
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    })
+})
+
+module.exports = router;
